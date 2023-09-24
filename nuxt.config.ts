ab431fca08edf82088341c7990735dab4a83adc9
Game Thanapon <134900556+TopcloudThanapon@users.noreply.github.com>
2023-09-24 16:46:59 +0700

    update firebase and ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: false,
+
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-gray-500 h-screen bg-black flex items-center justify-center">
+    Moonman Personal Website
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
